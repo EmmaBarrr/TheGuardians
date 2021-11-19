8e60592bea026bf7b6192f898bcc84ca8dca3086
RodoL0204 <rolega01@gmail.com>
2021-11-19 09:50:51 -0600

    added behaviors

---
+++ Assets/Scripts/BossStates/TeleportBehavior.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportBehavior : StateMachineBehaviour
+{
+    bool selected;
+    public bool hunter;
+    public float minHeight, maxHeight;
+    public int team;
+
+    public string[] state;
+
+    public float minTime;
+    public float maxTime;
+
+    private Transform playerPos;
+    private float timer;
+
+    Vector3 target;
+    public Vector3 targetDitance;
+
+    bool nullenemy;
+   
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        selected = false;
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+        if (!hunter)
+        {
+            int rx = Random.Range(-2, 2);
+
+            if (rx > 0)
+            {
+                target = new Vector3(playerPos.position.x + rx, animator.transform.position.y, playerPos.transform.position.z + 1.5f);
+            }
+            else if (rx <= 0)
+            {
+                target = new Vector3(playerPos.position.x + rx, animator.transform.position.y, playerPos.transform.position.z - 1.5f);
+            }
+        }
+        else if(hunter)
+        {
+            target = new Vector3(playerPos.position.x, animator.transform.position.y, playerPos.transform.position.z);
+        }
+
+        animator.transform.position = target;
+        if (!selected)
+        {
+            selected = true;
+            animator.SetTrigger(state[Random.Range(0, state.Length)]);
+        }
+        float minWidth = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 10)).x;
+        float maxWidth = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 10)).x;
+        animator.transform.position = new Vector3(animator.transform.position.x,
+            animator.transform.position.y,
+            Mathf.Clamp(animator.transform.position.z, minHeight, maxHeight));
+
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
---
+++ Assets/Scripts/BossStates/WalkBehavior.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkBehavior : StateMachineBehaviour
+{
+    bool selected;
+    public float minHeight, maxHeight;
+    public int team;
+
+    public string[] state;
+
+    public float minTime;
+    public float maxTime;
+
+    private Transform playerPos;
+    private float timer;
+    public float speed;
+
+    public bool charge;
+    public bool roll;
+
+    public bool retreat;
+    public float range;
+
+    bool nullenemy;
+
+    Vector3 target;
+    public Vector3 targetDitance;
+
+    float hForce;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        selected = false;
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+
+        target = new Vector3(playerPos.position.x, animator.transform.position.y, playerPos.transform.position.z);
+        
+        timer = Random.Range(minTime, maxTime);
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        timer -= Time.deltaTime;
+
+        if (!roll)
+        {
+            targetDitance = playerPos.position - animator.transform.position;
+            hForce = targetDitance.x / Mathf.Abs(targetDitance.x);
+
+            float zForce = Random.Range(-1, 2);
+
+            if (Mathf.Abs(targetDitance.x) < 1.3f)
+            {
+                zForce = targetDitance.z / Mathf.Abs(targetDitance.z);
+            }
+            if (Mathf.Abs(targetDitance.x) < range)
+            {
+                hForce = 0;
+            }
+            if (Mathf.Abs(targetDitance.z) < 0.1f)
+            {
+                zForce = 0;
+            }
+
+
+            target = new Vector3(animator.transform.position.x + hForce, animator.transform.position.y, animator.transform.position.z + zForce);
+        }
+
+        animator.transform.position = Vector3.MoveTowards(animator.transform.position, target, speed * Time.deltaTime);
+
+        if (!nullenemy)
+        {
+            if (retreat)
+            {
+                if (Mathf.Abs(targetDitance.x) > range && Mathf.Abs(targetDitance.z) < 0.2f)
+                {
+                    if (!selected)
+                    {
+                        selected = true;
+                        animator.SetTrigger("Attack");
+                    }
+
+                }
+            }
+            if (!retreat)
+            {
+                if (Mathf.Abs(targetDitance.x) < range && Mathf.Abs(targetDitance.z) < 0.2f)
+                {
+                        if (!selected)
+                        {
+                            selected = true;
+                            animator.SetTrigger("Attack");
+                        }
+
+                }
+            }
+        }
+
+        if (timer <= 0)
+        {
+            if (!selected)
+            {
+                selected = true;
+                animator.SetTrigger(state[Random.Range(0, state.Length)]);
+            }
+        }
+    }
+
+
+}
